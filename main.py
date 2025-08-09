3e04160998aba00917f39906fa2d7e2fb3925fdd
KR3915 <ondrej.brdek@gmail.com>
2025-08-09 19:35:09 +0200

    cleanup and comments

--- main.py
+++ main.py
@@ -4,37 +4,43 @@ import pygame
 TOOLBAR_HEIGHT = 50
 
 def main():
+# === Variables ===
     rows = 10
     cols = 10
     current_color = 2
     width = 500
     height = 500
-
+# === init ===
     screen = pygame.display.set_mode((width, height + TOOLBAR_HEIGHT))  # přidáme toolbar výšku
     grid = make_grid(rows, cols)
     print(grid)
     running = True
     clock = pygame.time.Clock()
-
+# ===== Main Loop =====
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            
+            # ==== Mouse Clicking ====
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = event.pos
                 if mouse_y < TOOLBAR_HEIGHT:
-                    # mouse click on the toolbar icon changes current color variable
+                    #White brush
                     if 10 < mouse_x < 50:
                         current_color = 1
+                    #Red Brush
                     elif 70 < mouse_x < 110:
                         current_color = 2
+                    #Blue Brush
                     elif 130 < mouse_x < 170:
                         current_color = 3
-                    elif 190 < mouse_x < 230:  # guma
+                    #Eraser
+                    elif 190 < mouse_x < 230:
                         current_color = 0
-                    elif 250 < mouse_x < 290:  # guma
+                    #Clear
+                    elif 250 < mouse_x < 290: 
                         grid = np.zeros((rows, cols))
+                    #info
                 else:
                     # Kliknutí v gridu - posuneme y o TOOLBAR_HEIGHT
                     col = int(mouse_x / (width / cols))
@@ -57,9 +63,11 @@ def main():
     pygame.quit()
     quit()
 
+#Making grind consisting of 0s
 def make_grid(rows, cols):
     return np.zeros((rows, cols))
 
+#draws grid
 def draw_grid(screen, grid):
     rows, cols = grid.shape
     screen_width, screen_height = screen.get_size()
@@ -77,6 +85,7 @@ def draw_grid(screen, grid):
             pygame.draw.rect(screen, (255, 255, 255), rect, 1)
     return cell_width, cell_height
 
+#updates colors depending on np array
 def fill_grid(grid, screen):
     rows, cols = grid.shape
     screen_width, screen_height = screen.get_size()
@@ -106,21 +115,21 @@ def fill_grid(grid, screen):
             pygame.draw.rect(screen, (255, 255, 255), rect, 1)
             pygame.display.update(rect)
 
+#draws toolbar
 def draw_toolbar(screen, current_color):
     pygame.draw.rect(screen, (50, 50, 50), (0, 0, screen.get_width(), TOOLBAR_HEIGHT))
     
     colors = [(255, 255, 255), (255, 0, 0), (0, 0, 255)]  # jen barvy
     x_positions = [10, 70, 130]
     
-    # vykreslení barevných tlačítek
+    # buttons render
     for i, color in enumerate(colors):
         rect = pygame.Rect(x_positions[i], 10, 40, 30)
         pygame.draw.rect(screen, color, rect)
         if current_color == i + 1:
             pygame.draw.rect(screen, (255, 255, 0), rect, 3)
-    ###################
-    ######buttons######
-    ###################
+    
+    # ====== buttons =======
     #NOTE: pygame.Rect(X,Y,Z,A) X is allways bigger by 60
     # --- eraser button ---
     eraser_rect = pygame.Rect(190, 10, 40, 30)
@@ -142,9 +151,10 @@ def draw_toolbar(screen, current_color):
     text_pos = text.get_rect(center=clear_rect.center)
     screen.blit(text, text_pos)
 
-    # zvýraznění gumy, pokud je vybraná (current_color == 0)
+    # highlight of selected button
     if current_color == 0:
         pygame.draw.rect(screen, (255, 255, 0), eraser_rect, 3)
+
 if __name__ == '__main__':
     pygame.init()
     main()
